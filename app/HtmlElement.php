da54fe4bf5ffa32b3654da7ed4981a0e0400d46b
Mario Sánchez <m.sanchez@cyberpuerta.mx>
2022-02-18 17:46:46 -0600

    Mejoras

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -4,6 +4,7 @@ namespace App;
 
 class Element 
 {
+    // using new feature from php 8
     public function __construct(
         public string $name,
         public array $attributes = [],
@@ -19,12 +20,24 @@ class Element
 
         $htmlAttributes = '';
         if ( $this->attributes ) {
-            foreach ($this->attributes as $key => $value) {
-                $htmlAttributes .= " $key='$value'";
+            foreach ($this->attributes as $attribute => $value) {
+                if ( is_numeric($attribute) ) {
+                    $htmlAttributes .= ' '.$value;
+                } else {
+                    $parse = htmlentities($value);
+                    $htmlAttributes .= " $attribute='{$parse}'";
+                }
             }
         }
 
         $result = "<{$this->name}{$htmlAttributes}>";
+
+        // si el elemento es void
+        // retorna el resultado de una sola vez
+        if ( in_array($this->name, ["hr", "br", "img", "input", "meta"])) {
+            return $result;
+        }
+
         $result .= $this->content;
         $result .= "<{$this->name}>";
 
@@ -33,8 +46,20 @@ class Element
 }
 
 
-$element = new Element(name: 'p', content: 'Este es el content');
-echo $element->render()."\n\n";
+$element = new Element(name: 'p', content: 'This is the content'); // new feature from php 8
+echo $element->render()."\n";
 
-$element = new Element('p', ['id' => 'mariossan', 'class' => 'escom'], 'Este es el content');
-echo $element->render();
+$element = new Element('p', ['id' => 'mariossan', 'class' => 'escom'], 'This is the content');
+echo $element->render()."\n";
+
+$element = new Element('img', ['src' => 'img/mariossan.png']);
+echo $element->render()."\n";
+
+$element = new Element('input');
+echo $element->render()."\n";
+
+$element = new Element('input', ['required']);
+echo $element->render()."\n";
+
+$element = new Element('input', ['required', 'title' => 'esta es una prueba de "title"']);
+echo $element->render()."\n";
